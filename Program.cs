34027f0859e724da1590ba4dd5a1927d9333df99
Sushimii <127798906+Sushimii@users.noreply.github.com>
2023-06-11 23:50:17 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ class Program
         {
             // Solicita ao usuario que digite um numero entre 1 e 10
             Console.Write("Digite um numero entre 1 e 10: ");
+            //Lendo o valor para adicionar a variavel
             num = int.Parse(Console.ReadLine());
 
             // Condição para verificar se o número está dentro do intervalo de 1 a 10
